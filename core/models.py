afbc1a85b2fe8a526c35bea58d99cc749c85dcc7
kokokojo2 <60895503+kokokojo2@users.noreply.github.com>
2021-01-14 13:23:49 +0200

    cart and order logic separated, additional functions added

--- core/models.py
+++ core/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
     title = models.CharField(max_length=100, verbose_name='Категория')
 
     def items(self):
-        return Item.objects.filter(category__parentCategory=self)
+        return Item.objects.filter(category__parent_category=self)
 
     def __str__(self):
         return self.title
@@ -15,18 +15,18 @@ class Category(models.Model):
 
 class SubCategory(models.Model):
     title = models.CharField(max_length=100, verbose_name='Подкатегория')
-    parentCategory = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subs')
+    parent_category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='subs')
 
     def __str__(self):
-        return self.parentCategory.title + ' | ' + self.title
+        return self.parent_category.title + ' | ' + self.title
 
 
 class Item(models.Model):
     title = models.CharField(max_length=100,verbose_name='Товар')
     price = models.FloatField(verbose_name='Цена', default=0)
     quantity = models.IntegerField(verbose_name='Количество товара на складе', default=0)
-    description = RichTextField(verbose_name='Описание',null=True)
-    category = models.ForeignKey(SubCategory,on_delete=models.CASCADE,default=None,null=True,related_name='items')
+    description = RichTextField(verbose_name='Описание', null=True)
+    category = models.ForeignKey(SubCategory, on_delete=models.CASCADE, default=None, null=True, related_name='items')
 
     def main_photo(self):
         return self.images.first()
@@ -37,33 +37,38 @@ class Item(models.Model):
 
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
-    item = models.ForeignKey(Item, related_name='images', on_delete=models.CASCADE,null=True)
+    item = models.ForeignKey(Item, related_name='images', on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Картинка для товара {self.item}'
 
 
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def get_items(self):
+        return self.items.all()
+
+    def total(self):
+        return sum([cart_item.item.price * cart_item.quantity for cart_item in self.get_items()])
+
+
 class CartItem(models.Model):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+
     quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return f'{self.item.title} в количестве: {self.quantity}'
 
 
-class Cart(models.Model):
-    ordered = models.BooleanField(default=False)
-
-    items = models.ManyToManyField(CartItem)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Клиент')
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
 
     shipping_address = models.TextField(verbose_name='Адрес получателя')
     ordered_date = models.DateTimeField(auto_now=True)
+    # спосіб оплати можна додати після уточнення, які саме способи доступні в магазині
 
     UNCONFIRMED = 1
     CONFIRMED = 2
@@ -83,12 +88,20 @@ class Order(models.Model):
 
     status = models.IntegerField(choices=ORDER_STATUS_CHOICES, default=UNCONFIRMED)
 
-    def items(self):
-        return self.cart.items.all()
+    def get_items(self):
+        return self.items.all()
 
     def total(self):
-        return sum([cart_item.item.price * cart_item.quanity for cart_item in self.items()])
+        return sum([order_item.item.price * order_item.quantity for order_item in self.get_items()])
 
     def __str__(self):
         return f'Заказ пользователя {self.user}'
+
+
+class OrderItem(models.Model):
+    item = models.ForeignKey(CartItem, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    quantity = models.PositiveIntegerField(verbose_name='Количество', default=1)
+
+
 
