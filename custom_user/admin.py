6ba6e95bf95288b013832f31b9a72d9eb0e025dc
kokokojo2 <60895503+kokokojo2@users.noreply.github.com>
2021-01-14 18:14:41 +0200

    user list_display configured

--- custom_user/admin.py
+++ custom_user/admin.py
@@ -5,6 +5,8 @@ from .models import CustomUser
 class UserAdmin(admin.ModelAdmin):
     exclude = ['username']
 
+    list_display = ('first_name', 'last_name', 'cellphone', 'is_active', 'is_confirmed')
+
     def get_readonly_fields(self, request, obj=None):
         if obj:
             return ["cellphone", 'first_name', 'last_name', 'date_joined', 'last_login', 'email']
